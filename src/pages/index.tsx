2df3c224402f1894b7214333673ac042fc3c5e32
andre silveira <andre.silveira@teamviewer.com>
2021-11-05 16:38:39 +0000

    add and config prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-const foo = "bla"
+const foo = 'bla'
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main >
+      <main>
         <h1>HOME</h1>
       </main>
     </div>
